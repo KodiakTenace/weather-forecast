c6312ad6e4c24c7f0318a54d268e069bb9587f3b
m.msan <m.msan@zeroandone.me>
2023-09-01 22:33:41 -0400

    add project + start with project infrastructure

--- index.ts
+++ index.ts
@@ -2,16 +2,89 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-
 // Create a new VPC for the application
-const vpc = new aws.ec2.Vpc("air-tek-vpc", {
-    cidrBlock: "10.0.0.0/16",
-    enableDnsHostnames: true,
-    enableDnsSupport: true,
-});
+const vpc = new awsx.ec2.Vpc("air-tek-vpc", {});
 
 // Create a security group for the application
 const securityGroup = new aws.ec2.SecurityGroup("air-tek-sg", {
-    vpcId: vpc.id,
-    // Add any necessary inbound rules here
+    vpcId: vpc.vpcId,
+    egress: [{
+        fromPort: 0,
+        toPort: 0,
+        protocol: "-1",
+        cidrBlocks: ["0.0.0.0/0"],
+        ipv6CidrBlocks: ["::/0"],
+    }],
 });
+
+const webUIRepository = new awsx.ecr.Repository("web-ui-repository", {});
+const webUIImage = new awsx.ecr.Image("web-ui-image", {
+    repositoryUrl: webUIRepository.url,
+    path: "./air-tek-weather-app/infra-web",
+});
+
+const webAPIRepository = new awsx.ecr.Repository("web-api-repository", {});
+const webAPIImage = new awsx.ecr.Image("web-api-image", {
+    repositoryUrl: webAPIRepository.url,
+    path: "./air-tek-weather-app/infra-api",
+});
+
+const cluster = new aws.ecs.Cluster("air-tek-cluster", {});
+
+
+// Create an ALB associated with the default VPC for this region.
+const alb = new awsx.lb.ApplicationLoadBalancer("web-traffic", {
+    subnetIds: vpc.publicSubnetIds,
+    listener: { port: 80 },
+    defaultTargetGroup: {
+        port: 3000,
+    },
+});
+
+// Listen to HTTP traffic on port 80.
+const listener = alb.listeners;
+
+// Export the resulting URL so that it's easy to access.
+
+// Create Fargate services for Web UI
+const webUIService = new awsx.ecs.FargateService("web-ui-service", {
+    cluster: cluster.arn,
+    networkConfiguration: {
+      subnets: vpc.privateSubnetIds,
+      securityGroups: [securityGroup.id]
+    },
+    desiredCount: 1,
+    taskDefinitionArgs: {
+        container: {
+            name: 'web-ui-container',
+            image: webUIImage.imageUri,
+            cpu: 512,
+            memory: 128,
+            essential: true,
+            portMappings: [{
+                targetGroup: alb.defaultTargetGroup,
+            }]
+        },
+    },
+});
+
+// Create Fargate services for Web UI
+const webSPIService = new awsx.ecs.FargateService("web-api-service", {
+    cluster: cluster.arn,
+    networkConfiguration: {
+        subnets: vpc.privateSubnetIds,
+        securityGroups: [securityGroup.id]
+    },
+    desiredCount: 1,
+    taskDefinitionArgs: {
+        container: {
+            name: 'web-api-container',
+            image: webAPIImage.imageUri,
+            cpu: 512,
+            memory: 128,
+            essential: true,
+        },
+    },
+});
+
+export const endpoint = alb.loadBalancer.dnsName;
